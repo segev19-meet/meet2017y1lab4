eac5d8d3065acbac5a7dc42a7206d91d99b1899b
segev19-meet <segev19@meet.mit.edu>
2017-07-26 12:25:44 +0300

    Stopping here

---
+++ "turtle_party.py
@@ -0,0 +1,12 @@
+strawberries=39
+is_weekend=True
+if is_weekend:
+    if strawberries >39:
+       print ('Fun')
+    else:
+       print('not Fun')
+else:
+    if strawberries>39 and strawberries<61:
+        print('Fun')
+    else:
+        print('Not Fun')
---
+++ fruite_sorter.py
@@ -0,0 +1,10 @@
+new_fruite=input('what fuite am i sorting?')
+if new_fruite=='apples':
+    print('apples')
+elif new_fruite=='oranges':
+    print('oranges')
+elif new_fruite=='olives':
+    print('olives')
+
+else:
+    print('error! I do not recognise this fruit!')
